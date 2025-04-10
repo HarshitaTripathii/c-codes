ee944c7fa8699ddd5e33a00fc4e99440d9e2f143
HarshitaTripathii <148742116+HarshitaTripathii@users.noreply.github.com>
2025-04-10 13:59:18 +0000

    basics done

--- cpp codes/oopConcept.cpp
+++ cpp codes/oopConcept.cpp
@@ -1,28 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
-class details{
-    // data memebrs : attribute
-    string name;
-    int age;
-    float cgpa;
-    vector<string> lang;
+class Details{
+    public:
+        // data memebrs : attribute
+        string name;
+        int age;
+        float cgpa;
+        vector<string> lang;
 
-    // member function : methods
-    void displayEachDetail()
-    {
+        // member function : methods
+        void displayEachDetail()
+        {
         cout<< "Name is "<< name<< " of age "<<age<< " and cg is "<< cgpa<< endl;
         cout << "Languages Known is ";
         for(string l : lang)
         {
-            cout << l<< " ";
+        cout << l<< " ";
         }
         cout << endl;
 
-    }
+        }
+    private:
+        int package;
 
-}
+};
 int main()
 {
-    cout<< "hello";
+    // cout<< "hello";
+    Details detail1;  // object detail has been created
+    detail1.name="Harshita Tripathi";
+    detail1.age=21;
+    detail1.cgpa=8.75;
+    detail1.lang={"English", "Hindi"};
+
+    Details detail2;  // object detail has been created
+    detail2.name="Arya Tripathi";
+    detail2.age=23;
+    detail2.cgpa=9.75;
+    detail2.lang={"English", "Hindi"};
+
+    detail1.displayEachDetail();
+    detail2.displayEachDetail();
     return 0;
 }
