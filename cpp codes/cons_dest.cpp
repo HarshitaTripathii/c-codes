0b4af56482f6eaf3851a1eb019ae3c6a4008f335
HarshitaTripathii <148742116+HarshitaTripathii@users.noreply.github.com>
2025-04-10 17:22:11 +0000

    pbj allocated in heap Way , destructor callled with and without being defined

--- cpp codes/cons_dest.cpp
+++ cpp codes/cons_dest.cpp
@@ -13,8 +13,33 @@ public:
     // No destructor defined
 };
 
+class Sample1 {
+public:
+    Sample1() { std::cout << "Constructor called." << std::endl; }
+    ~Sample1() { std::cout << "Destructor called." << std::endl; }
+};
+
 int main() {
-    Sample* s = new Sample(); // Constructor called
+   
+
+    return 0;
+}
+
+int main() {
+    // for class Sample2
+
+    {
+        Sample1 s1; // Constructor called, s1 goes out of scope -> Destructor called
+    }
+
+    Sample* s2 = new Sample(); // Constructor called
+    delete s2; // Destructor called
+
+
+
+
+// for class Sample1
+    Sample1* s = new Sample1(); // Constructor called
 
     delete s; // Memory for 's' is deallocated, but no destructor is called
 
