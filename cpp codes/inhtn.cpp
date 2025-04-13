1ea73f41c9f66ccbf81882a1745d2a18f2e19748
HarshitaTripathii <148742116+HarshitaTripathii@users.noreply.github.com>
2025-04-13 06:07:47 +0000

    final, override, protected, inheritence

--- cpp codes/inhtn.cpp
+++ cpp codes/inhtn.cpp
@@ -7,23 +7,24 @@ class Tea{
         string teaName;
         int serve;
     // constructor
-    Tea(string t, int s): teaName(t), serve(s){
-        cout<< "constructor has been called" << endl;
-    }
+    public:
+        Tea(string t, int s): teaName(t), serve(s){
+            cout<< "constructor has been called" << endl;
+        }
 
-    virtual void brew() const{
-        cout<< " brewing"<< teaName<< endl;
+        virtual void brew() const{
+            cout<< " brewing"<< teaName<< endl;
 
-    }
-    virtual void make() const{
-        cout<< " serving"<< serve<< endl;
+        }
+        virtual void make() const{
+            cout<< " serving"<< serve<< endl;
 
-    }
-    virtual ~Tea()
-    {
-        cout<< "destructor has been called"<< endl;
-    }
-        
+        }
+        virtual ~Tea()
+        {
+            cout<< "destructor has been called"<< endl;
+        }
+            
 };
 
 class GreenTea: public Tea
@@ -47,7 +48,7 @@ class MasalaTea: public Tea
         MasalaTea(int serve): Tea("Masala Tea", serve){
             cout<< "Masala tea made"<< endl;
         }
-        void brew() const override final {
+        void brew() const override final {    // This method cannot be overridden as "final" keyword is present
             cout<< " brewing"<< teaName<< endl;;
         }
         ~MasalaTea()
@@ -56,17 +57,31 @@ class MasalaTea: public Tea
         }
 };
 
-class ExtraMasalaTea: public MasalaTeaTea
+class ExtraMasalaTea: public MasalaTea
 {
     public:
-        ExtraMasalaTea(int serve): MasalaTea("Masala Tea", serve){
+        ExtraMasalaTea(int serve): MasalaTea( serve){
             cout<< "Masala tea made"<< endl;
         }
-        void brew() const override  {
-            cout<< " brewing and using it in Extra masala"<< teaName<< endl;;
-        }
-        ~MasalaTea()
+        // void brew() const override  {
+        //     cout<< " brewing and using it in Extra masala"<< teaName<< endl;;
+        // }
+        ~ExtraMasalaTea()
         {
             cout<< "destructor has been called"<< endl;
         }
 };
+
+int main()
+{
+    Tea* Tea1=new GreenTea(3);
+    Tea* Tea2=new MasalaTea(7);
+    // Tea* Tea3=new ExtraMasalaTea(7);
+    Tea1->brew();
+    Tea2->brew();
+    // Tea3->brew();
+
+    delete Tea1;
+    delete Tea2;
+return 0;
+}
